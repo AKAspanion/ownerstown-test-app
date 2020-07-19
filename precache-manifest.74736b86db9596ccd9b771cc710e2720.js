8908fccb32f5965f4b5d58b791ee8b6775a50787
Ankit Kumar <panditankit1995@gmail.com>
2020-07-19 11:06:18 +0530

    Updates

---
+++ precache-manifest.74736b86db9596ccd9b771cc710e2720.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e449c0978c8c13db4506dcf857f6a5",
+    "url": "/ownerstown-test-app/index.html"
+  },
+  {
+    "revision": "543af8b0412e95907c21",
+    "url": "/ownerstown-test-app/static/css/main.316d7af8.chunk.css"
+  },
+  {
+    "revision": "bfc9f0bd78c77e1fe085",
+    "url": "/ownerstown-test-app/static/js/2.ec5d8b79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ownerstown-test-app/static/js/2.ec5d8b79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543af8b0412e95907c21",
+    "url": "/ownerstown-test-app/static/js/main.a8278a39.chunk.js"
+  },
+  {
+    "revision": "8ebce8087a86136125b3",
+    "url": "/ownerstown-test-app/static/js/runtime-main.67bfa2bf.js"
+  }
+]);
